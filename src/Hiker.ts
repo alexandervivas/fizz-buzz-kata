11a466b16337e3a4f7bdef32610d48656894e989
pslaulhe <pablo@lendsum.com>
2023-12-20 14:56:53 +0100

    fourth commit

--- src/Hiker.ts
+++ src/Hiker.ts
@@ -1,5 +1,7 @@
 export class Hiker {
 
+  // It's a function that will print the number but everytime it's divided by 3 you will see in the console Fizz and when it's divided by 5 you will see in the console Buzz
+  // and when it's divided by 3 and 5 you will see in the console FizzBuzz. Otherwise it will just print the number
   public printNumber(num: number) {
    if(num % 3 == 0 && num % 5 == 0) {
      console.log("FizzBuzz");
@@ -12,6 +14,7 @@ export class Hiker {
    }
   }
 
+  // It's a function that will go from 1 to 100 and for every number it will call the function printNumber
   public printFizzBuzz() {
     for (let i = 1;i <= 100; i++) {
       this.printNumber(i);
